41603a6347ca12542bcc7fdf2694cc5183f8a96e
spandan-kumar <spandankumar9@gmail.com>
2024-09-07 22:15:24 +0530

    Initial commit

---
+++ app/page.tsx
@@ -0,0 +1,10 @@
+import Image from "next/image";
+import { Chipin } from "@/components/component/chipin";
+
+export default function Home() {
+  return (
+    <main>
+      <Chipin />
+    </main>
+  );
+}
